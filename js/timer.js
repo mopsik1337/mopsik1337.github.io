b63631efb543236aafb28b81b87f2ac6ccd7bb43
Milan <76396495+JaerTheBest@users.noreply.github.com>
2023-05-11 19:51:44 +0600

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,27 +1,23 @@
-// задаем дату окончания таймера
 const endDate = new Date('2023-05-27T20:00:00').getTime();
+let requestId;
 
-// обновляем таймер каждую секунду
-const timer = setInterval(function() {
-
-  // получаем текущее время
+function updateTimer() {
   const now = new Date().getTime();
-
-  // вычисляем оставшееся время
   const remainingTime = endDate - now;
 
-  // вычисляем дни, часы, минуты и секунды
   const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
   const hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
 
-  // отображаем таймер на странице
   document.getElementById('countdown').innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 
-  // если время вышло, останавливаем таймер
   if (remainingTime < 0) {
-    clearInterval(timer);
     document.getElementById('countdown').innerHTML = 'Время вышло!';
+    cancelAnimationFrame(requestId);
+  } else {
+    requestId = requestAnimationFrame(updateTimer);
   }
-}, 1000);
+}
+
+updateTimer();
